18850dcb594d610cbcfcdef676fdb30c5de94596
Vincent Chan <vincent.chan@eigentech.com>
2021-01-15 18:07:03 +0000

    working with the highlighted text

--- out/extension.js
+++ out/extension.js
@@ -10,77 +10,94 @@ const bing_search = bingSearch.bing_search;
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
-    context.subscriptions.push(vscode.commands.registerCommand('clippy.helloWorld', () => {
-        const clippy_js_on_disk = vscode.Uri.file(path.join(context.extensionPath, "src/clippy/build/clippy.min.js"));
-        const clippy_css_on_disk = vscode.Uri.file(path.join(context.extensionPath, "src/clippy/build/clippy.css"));
-        // Create and show a new webview
-        const panel = vscode.window.createWebviewPanel('clippy', // Identifies the type of the webview. Used internally
-        'Clippy', // Title of the panel displayed to the user
-        {
-            viewColumn: vscode.ViewColumn.Beside,
-            preserveFocus: true
-        }, {
-            enableScripts: true,
-            localResourceRoots: [
-                context.extensionUri
-            ],
-        } // Webview options. More on these later.
-        );
-        //@ts-ignore
-        const clippy_js = panel.webview.asWebviewUri(clippy_js_on_disk);
-        //@ts-ignore
-        const clippy_css = panel.webview.asWebviewUri(clippy_css_on_disk);
-        // And set its HTML content
-        panel.webview.html = getWebviewContent(clippy_js, clippy_css);
+    let clippyPanel = null;
+    context.subscriptions.push(vscode.window.onDidChangeTextEditorSelection((event) => {
+        let text = event.textEditor.document.getText(event.selections[0]);
+        console.log(text);
+        if (text) {
+            if (clippyPanel)
+                clippyPanel.dispose();
+            clippyPanel = clippy(context, text);
+        }
     }));
 }
 exports.activate = activate;
-function getWebviewContent(clippy_js, clippy_css) {
+function clippy(context, text) {
+    // Create and show a new webview
+    const panel = vscode.window.createWebviewPanel('clippy', // Identifies the type of the webview. Used internally
+    'Clippy', // Title of the panel displayed to the user
+    {
+        viewColumn: vscode.ViewColumn.Beside,
+        preserveFocus: true
+    }, {
+        enableScripts: true,
+        localResourceRoots: [
+            context.extensionUri
+        ],
+    });
+    const clippy_js_on_disk = vscode.Uri.file(path.join(context.extensionPath, "src/clippy/build/clippy.min.js"));
+    const clippy_css_on_disk = vscode.Uri.file(path.join(context.extensionPath, "src/clippy/build/clippy.css"));
+    //@ts-ignore
+    const clippy_js = panel.webview.asWebviewUri(clippy_js_on_disk);
+    //@ts-ignore
+    const clippy_css = panel.webview.asWebviewUri(clippy_css_on_disk);
+    // And set its HTML content
+    panel.webview.html = getWebviewContent(clippy_js, clippy_css, text);
+    return panel;
+}
+function getWebviewContent(clippy_js, clippy_css, text) {
     return `<!DOCTYPE html>
-  <html lang="en">
-  <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Clippy</title>
-      <link rel="stylesheet" type="text/css" href="${clippy_css}" media="all">
-  </head>
-  <body>
-    <label for="name">Imagine this is the editor</label>
-	<input type="text" id="name" />
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Clippy</title>
+			<link rel="stylesheet" type="text/css" href="${clippy_css}" media="all">
+		</head>
+		<body>
+			<input type="hidden" id="name" value="${text}"/>
 
-    <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
-    <script src="${clippy_js}"></script>
-    <!-- Init script -->
-	<script type="text/javascript">
-		let input = document.querySelector('#name');
-
-		help_options = {
-			"use": "Looks like you're trying to use hooks!",
-			"for": "Looks like you're trying to write a for loop!",
-			"switch": "Looks like you're trying to write a switch statement!",
-		}
-
-		clippy.load('Clippy', function(agent) {
-			agent.animate();
-			input.addEventListener('keypress', (e) => {
-				if (e.code === "Space") {
-					speak(agent, e.target.value);
+			<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
+			<script src="${clippy_js}"></script>
+			<!-- Init script -->
+			<script type="text/javascript">
+				help_options = {
+					"use": "Looks like you're trying to use hooks!",
+					"for": "Looks like you're trying to write a for loop!",
+					"switch": "Looks like you're trying to write a switch statement!",
 				}
-			});
-		});
 
-      const speak = (agent, query) => {
-        words = query.split(" ")
-        // Always get the last typed word
-        query_output = help_options[words[words.length - 1]]
-        if (query_output) {
-          agent.play('Searching');
-          agent.speak(query_output);
-        }
-      }
-    </script>
-  </body>
-</html>`;
+				let input = document.querySelector('#name');
+
+				clippy.load('Clippy', function(agent) {
+					agent.animate();
+					speak(agent, input.value);
+				});
+
+			const speak = (agent, query) => {
+				words = query.split(" ")
+				
+				// Loop over the selection in reverse and find the first match
+				query_output = ''
+				for (let i = words.length - 1; i >= 0; i--) {
+					console.log(i)
+					if (Object.keys(help_options).indexOf(words[i].toLowerCase()) !== -1) {
+						query_output = help_options[words[i].toLowerCase()]
+						break;
+					}
+				}
+
+				if (query_output) {
+					agent.play('Searching');
+
+					// Bing search
+
+					agent.speak(query_output);
+				}
+			}
+			</script>
+		</body>
+		</html>`;
 }
 // this method is called when your extension is deactivated
 function deactivate() { }
