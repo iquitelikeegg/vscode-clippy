04d1ca4b986145e680b16623ea3c3869ecee11b5
Vincent Chan <vincent.chan@eigentech.com>
2021-01-15 12:33:39 +0000

    will now roundrobin a set of ms support sites

--- bing_search.js
+++ bing_search.js
@@ -1,8 +1,25 @@
 const bing_search = (() => {
     const bingUrl = "https://bing.com/search?q="
-    const queryPrefix = "site:support.microsoft.com "
+    const msSupportSites = [
+        "support.microsoft.com",
+        "support.microsoft.com/en-us/microsoft-365",
+        "support.microsoft.com/en-us/outlook",
+        "support.microsoft.com/en-us/word",
+        "support.microsoft.com/en-us/excel",
+        "support.microsoft.com/en-us/powerpoint",
+        "support.microsoft.com/en-us/teams",
+        "docs.microsoft.com/en-us/windows"
+    ]
+    let roundRobinPointer = 0
 
     function generate_link(input) {
+        let queryPrefix = `site:${msSupportSites[roundRobinPointer]} `
+
+        roundRobinPointer++
+
+        if (roundRobinPointer > msSupportSites.length - 1)
+            roundRobinPointer = 0
+
         return bingUrl + encodeURI(queryPrefix + input)
     }
 
