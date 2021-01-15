f79b465b1b8166d87ededa33838c87f8801f5ed0
eimantas.zilis <eimantas.zilis@eigentech.com>
2021-01-15 13:54:50 +0000

    open new tab with extension

--- src/extension.ts
+++ src/extension.ts
@@ -8,24 +8,18 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "clippy" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('clippy.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		// vscode.window.showInformationMessage(bing_search.generate_link('Hello World from Clippy!'));
-		vscode.window.showInformationMessage('Hello World from Clippy! Random hello');
-	});
-
-	context.subscriptions.push(disposable);
-}
+	context.subscriptions.push(
+	  vscode.commands.registerCommand('clippy.helloWorld', () => {
+		// Create and show a new webview
+		const panel = vscode.window.createWebviewPanel(
+		  'clippy', // Identifies the type of the webview. Used internally
+		  'Clippy', // Title of the panel displayed to the user
+		  vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+		  {} // Webview options. More on these later.
+		);
+	  })
+	);
+  }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
